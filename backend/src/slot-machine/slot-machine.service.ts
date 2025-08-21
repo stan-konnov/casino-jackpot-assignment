e678c7ccacb5fa2e2a9b38f08cdd6051d559f0cc
Stan Konnov <stanis.konnov@gmail.com>
2025-08-21 15:41:24 +0200

    feature: Working on spinning logic in the Slot Machine Service.

--- backend/src/slot-machine/slot-machine.service.ts
+++ backend/src/slot-machine/slot-machine.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
+
+import { SlotSymbol } from '@src/slot-machine/slot-symbol.enum';
 import { DatabaseService } from '@src/database/database.service';
 
 @Injectable()
 export class SlotMachineService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  private readonly symbols: SlotSymbol[];
+
+  constructor(private readonly databaseService: DatabaseService) {
+    this.symbols = Object.values(SlotSymbol);
+  }
+
+  private spin(): [SlotSymbol, SlotSymbol, SlotSymbol, SlotSymbol] {
+    return [
+      this.getRandomSymbol(),
+      this.getRandomSymbol(),
+      this.getRandomSymbol(),
+      this.getRandomSymbol(),
+    ];
+  }
+
+  private getRandomSymbol = (): SlotSymbol =>
+    this.symbols[Math.floor(Math.random() * this.symbols.length)] as SlotSymbol;
 }

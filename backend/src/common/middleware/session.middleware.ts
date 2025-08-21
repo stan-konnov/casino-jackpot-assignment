51bcbebe21e4d78b4d552a71b6d630e8bdecbba2
Stan Konnov <stanis.konnov@gmail.com>
2025-08-21 14:53:24 +0200

    feature: Introduce middleware that parses incoming client IP and creates a user session and attaches it to the request object. Introduce custom typing.

---
+++ backend/src/common/middleware/session.middleware.ts
@@ -0,0 +1,42 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+import { DatabaseService } from '@src/database/database.service';
+import { RequestWithSession } from '@src/common/types/request.with.session';
+
+/**
+ * Middleware to handle user sessions based on IP address.
+ * It checks if a session exists for the given IP, and if not, creates a new session with default credits.
+ */
+@Injectable()
+export class SessionMiddleware implements NestMiddleware {
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async use(request: Request, _response: Response, next: NextFunction): Promise<void> {
+    const ip = String(
+      request.headers['x-forwarded-for']?.toString().split(',')[0] || request.socket.remoteAddress,
+    );
+
+    let session = await this.databaseService.session.findFirst({
+      where: { ip },
+    });
+
+    if (!session) {
+      session = await this.databaseService.session.create({
+        data: {
+          ip,
+          credits: Number(this.configService.get('DEFAULT_ALLOCATED_CREDITS')),
+        },
+      });
+    }
+
+    // Attach session to request with custom type
+    (request as RequestWithSession).session = session;
+
+    next();
+  }
+}

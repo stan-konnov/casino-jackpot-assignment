9998709f70792c7b204c03d965a5c1df3fd06496
Stan Konnov <stanis.konnov@gmail.com>
2025-08-21 23:18:27 +0200

    chore: 'Enumenize' the routes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { ReactElement } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+3
 import { SlotMachinePage } from '@src/pages/SlotMachinePage';
+import { AppRoutes } from '@src/common/routes';
 
 const App = (): ReactElement => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/slot-machine" replace />} />
-        <Route path="/slot-machine" element={<SlotMachinePage />} />
+        <Route path={AppRoutes.ROOT} element={<Navigate to={AppRoutes.SLOT_MACHINE} replace />} />
+        <Route path={AppRoutes.SLOT_MACHINE} element={<SlotMachinePage />} />
       </Routes>
     </BrowserRouter>
   );

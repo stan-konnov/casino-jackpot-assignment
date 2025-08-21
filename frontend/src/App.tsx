427b5dbf7d9dc1d18e66c6ace2f576b681a64d34
Stan Konnov <stanis.konnov@gmail.com>
2025-08-21 12:45:08 +0200

    chore: Basic setup for the frontend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+
+const App = (): ReactElement => {
+  return (
+    <div>
+      <h1>Welcome to the Casino Jackpot Assignment</h1>
+      <p>This is a placeholder for application content.</p>
+    </div>
+  );
+};
+
+export default App;

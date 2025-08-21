788548a68651fdcd01e9ee8cfd3209ca1a036131
Stan Konnov <stanis.konnov@gmail.com>
2025-08-21 15:54:22 +0200

    feature: Implement spin and reward mechanism without cheating rules. Let's first communicate it back via API.

--- backend/src/slot-machine/slot-machine.service.ts
+++ backend/src/slot-machine/slot-machine.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { SlotSymbol } from '@src/slot-machine/slot-symbol.enum';
 import { DatabaseService } from '@src/database/database.service';
+import { InsufficientCreditsError, SessionDoesNotExistError } from '@src/slot-machine/errors';
 
 @Injectable()
 export class SlotMachineService {
@@ -11,7 +12,53 @@ export class SlotMachineService {
     this.symbols = Object.values(SlotSymbol);
   }
 
-  private spin(): [SlotSymbol, SlotSymbol, SlotSymbol, SlotSymbol] {
+  public async play(sessionId: string): Promise<[SlotSymbol, SlotSymbol, SlotSymbol, SlotSymbol]> {
+    const session = await this.databaseService.session.findFirst({ where: { id: sessionId } });
+
+    if (!session) {
+      throw new SessionDoesNotExistError(`Session with ID ${sessionId} does not exist.`);
+    }
+
+    if (session.credits === 0) {
+      throw new InsufficientCreditsError(`Session with ID ${sessionId} has insufficient credits.`);
+    }
+
+    await this.databaseService.session.update({
+      where: { id: sessionId },
+      data: { credits: { decrement: 1 } },
+    });
+
+    const slots = this.spinTheSlots();
+
+    const win = slots.every((symbol) => symbol === slots[0]);
+    let reward = 0;
+
+    if (win) {
+      switch (slots[0]) {
+        case SlotSymbol.CHERRY:
+          reward = 10;
+          break;
+        case SlotSymbol.LEMON:
+          reward = 20;
+          break;
+        case SlotSymbol.ORANGE:
+          reward = 30;
+          break;
+        case SlotSymbol.WATERMELON:
+          reward = 40;
+          break;
+      }
+
+      await this.databaseService.session.update({
+        where: { id: sessionId },
+        data: { credits: { increment: reward } },
+      });
+    }
+
+    return slots;
+  }
+
+  private spinTheSlots(): [SlotSymbol, SlotSymbol, SlotSymbol, SlotSymbol] {
     return [
       this.getRandomSymbol(),
       this.getRandomSymbol(),
